c0b191cea388cdd802fe7608a159c1bf48cf825c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-01 11:49:46 -0300

    add studentService property to the GradebookController

--- src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springmvc.controller;
 
 import com.luv2code.springmvc.models.*;
+import com.luv2code.springmvc.service.StudentAndGradeService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,16 +14,19 @@ public class GradebookController {
 	@Autowired
 	private Gradebook gradebook;
 
+	@Autowired
+	private StudentAndGradeService studentService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getStudents(Model m) {
+		Iterable<CollegeStudent> students = this.studentService.getGradebook();
+		m.addAttribute("students", students);
 		return "index";
 	}
 
-
 	@GetMapping("/studentInformation/{id}")
-		public String studentInformation(@PathVariable int id, Model m) {
+	public String studentInformation(@PathVariable int id, Model m) {
 		return "studentInformation";
-		}
+	}
 
 }
